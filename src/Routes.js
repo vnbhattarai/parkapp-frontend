f93831fc972bc6da9fcdcd85ba48319d52327898
Vinaya Bhattarai <vnbhattarai@protonmail.ch>
2017-07-28 23:56:54 +0300

    feat: Add basic redirection for /dashboard route

--- src/Routes.js
+++ src/Routes.js
@@ -1,17 +1,36 @@
 import React from "react";
-import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
 import Application from "./App";
 import LoginPage from "./containers/LoginPage";
 import ReservationPage from "./containers/ReservationPage";
 import Header from "./components/Header";
 import SignupPage from "./containers/SignupPage";
+import Dashboard from "./containers/Dashboard";
+
+const Auth = {
+  isAuthenticated: true
+};
+
+const PrivateRoute = ({ component: Component, ...rest }) =>
+  <Route
+    {...rest}
+    render={props =>
+      Auth.isAuthenticated
+        ? <Component {...props} />
+        : <Redirect
+            to={{
+              pathname: "/login",
+              state: { from: props.location }
+            }}
+          />}
+  />;
 
 const Routes = () =>
   <BrowserRouter>
     <div>
       <Header />
       <Switch>
-        <Route exact path="/" component={Application} />
+        <PrivateRoute exact path="/" component={Application} />
         <Route path="/login" component={LoginPage} />
         <Route path="/signup" component={SignupPage} />
         <Route path="/reservation" component={ReservationPage} />
@@ -20,3 +39,4 @@ const Routes = () =>
   </BrowserRouter>;
 
 export default Routes;
+// <Route exact path="/" component={Application} />
