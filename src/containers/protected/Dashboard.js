967db95f31dca1bf6e8081b6ab47f62cc61bf21f
Vinaya Bhattarai <vnbhattarai@protonmail.ch>
2017-07-30 19:53:30 +0300

    feat: Sample reservation table view

--- src/containers/protected/Dashboard.js
+++ src/containers/protected/Dashboard.js
@@ -1,7 +1,50 @@
 import React from "react";
 
-const Dashboard = () => {
-  return <div>Only registered should see this</div>;
+const Dashboard = props => {
+  return (
+    <div>
+      <div className="row">
+        <h5>
+          Hi {props.user}
+        </h5>
+      </div>
+      <div className="row">
+        <div className="col s12" />
+        <h5 style={{ textAlign: "center" }}>Your upcoming reservations</h5>
+        <table>
+          <thead>
+            <tr>
+              <th>Date</th>
+              <th>Starting Time</th>
+              <th>Ending Time</th>
+              <th>Parking space</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            <tr>
+              <td>2017-01-03</td>
+              <td>12:00</td>
+              <td>13:00</td>
+              <td>A12</td>
+            </tr>
+            <tr>
+              <td>2017-04-10</td>
+              <td>16:00</td>
+              <td>17:00</td>
+              <td>B8</td>
+            </tr>
+            <tr>
+              <td>2017-06-10</td>
+              <td>08:00</td>
+              <td>10:00</td>
+              <td>E8</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 };
 
 export default Dashboard;
