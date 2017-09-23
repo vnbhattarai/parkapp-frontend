0e6dbfb81c760ffa69ea406e9c33de54bd6f185c
Vinaya Bhattarai <vnbhattarai@protonmail.ch>
2017-09-23 16:03:14 +0300

    feat: Remove redundant files

--- src/index.js
+++ src/index.js
@@ -1,19 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./containers/App";
-import { Provider } from "react-redux";
-import { configureStore } from "./store/configureStore";
-// import registerServiceWorker from "./registerServiceWorker";
-import injectTapEventPlugin from "react-tap-event-plugin";
 
-injectTapEventPlugin();
-const store = configureStore();
+// Components import
+import App from "./components/App";
 
-ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById("root")
-);
-
-// registerServiceWorker();
+ReactDOM.render(<App />, document.getElementById("root"));
